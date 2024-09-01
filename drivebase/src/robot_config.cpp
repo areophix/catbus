952f5269203750778c0858b56e518488bc8600b6
areophix <161530331+areophix@users.noreply.github.com>
2024-09-01 12:13:27 -0500

    build file cleanup
    build dirs added to ignore file. all build files removed

--- drivebase/src/robot_config.cpp
+++ drivebase/src/robot_config.cpp
@@ -11,10 +11,10 @@ brain  Brain;
 controller Controller = controller(primary); // device names are usually capitilized cause lowercase ones are keywords
                                              // and the compiler will freak out cause it cant tell them apart
                                              
-motor LF = motor(PORT7, ratio6_1, false); // brain port the device is plugged into, cartridge ratio (different 
+motor LF = motor(PORT20, ratio6_1, true); // brain port the device is plugged into, cartridge ratio (different 
                                          // motors have diff cartridges for speed or strength), true = reversed, false = normal
-motor LB = motor(PORT5, ratio6_1, true);
+motor LB = motor(PORT10, ratio6_1, true);
 
-motor RF = motor(PORT3, ratio6_1, false);
-motor RB = motor(PORT9, ratio6_1, true);
+motor RF = motor(PORT12, ratio6_1, false);
+motor RB = motor(PORT1, ratio6_1, false);
 // the false ones are reversed due to their orientation you'll see why next week when we build it :D
