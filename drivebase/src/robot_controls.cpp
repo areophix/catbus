2f0c9102da21810241d2ec675cba81f1f4546b1d
areophix <Morgan.deng@icloud.com>
2025-01-14 23:15:27 -0600

    start pneumatics & adjustments
    clamp, adjust speeds & motor orientations, still need to add arm pistons & update climb & devices

--- drivebase/src/robot_controls.cpp
+++ drivebase/src/robot_controls.cpp
@@ -1,14 +1,18 @@
 #include "robot_controls.h"
+#include <iostream>
 
 using namespace vex;
+using namespace std;
 
 void usercontrol(void) {  
   brakeType driveBrake = coast; // ???
   //devices_check(); 
   Arm_State arm_state = intake;
+  int clamp_state = 0; //closed  
   while (1) // keeps checking the controller in a loop to get updates on whether or not its being moved
   { // the drive_brake, voltDrive, and driveCurve methods are in drive.cpp
     arm_state = arm_controls(arm_state);
+    clamp_state = clamp_controls(clamp_state);
     if ((abs(Controller.Axis3.position(pct)) < 2) && (abs(Controller.Axis2.position(pct)) < 2)) {
       drive_brake(driveBrake);
     }
@@ -21,7 +25,6 @@ void usercontrol(void) {
       }
     }
     this_thread::sleep_for(15); // wait so it doesn't burn out from constantly running poor lil guy
-
   }
 }
 
@@ -36,36 +39,46 @@ void devices_check() {
   }
 }
 
+int clamp_controls(int state) {
+  if(Controller.ButtonRight.pressing()) {
+    if (state) { 
+      clamp.close();
+      return 0;
+    }
+    else {
+      clamp.open();
+      return 1;
+    }
+  } else {
+    //hey stupid, no button control must maintain state
+    return state;
+  }
+}
+
 Arm_State arm_controls(Arm_State state) {
     if(state == intake) {
         if(Controller.ButtonX.pressing()) {
-          Brain.Screen.print("switched to arm");
+          Brain.Screen.print("switched to climb");
           return arm;
         }
         else if(Controller.ButtonR1.pressing()) {
-          intake_wheels.spin(directionType::fwd, 50, velocityUnits::pct);
-          conveyor.spin(directionType::fwd, 50, velocityUnits::pct);
+          intake_wheels.spin(directionType::fwd, 75, velocityUnits::pct);
+          conveyor.spin(directionType::fwd, 100, velocityUnits::pct);
         }
         else if(Controller.ButtonR2.pressing()) {
-          intake_wheels.spin(directionType::fwd, -50, velocityUnits::pct);
-          conveyor.spin(directionType::fwd, -50, velocityUnits::pct);
+          intake_wheels.spin(directionType::fwd, -75, velocityUnits::pct);
+          conveyor.spin(directionType::fwd, -100, velocityUnits::pct);
         }
         else if(Controller.ButtonL1.pressing()) {
-          intake_arm.spin(directionType::fwd, 50, velocityUnits::pct);
+          intake_arm.spin(directionType::fwd, 25, velocityUnits::pct);
         }
         else if(Controller.ButtonL2.pressing()) {
-          intake_arm.spin(directionType::fwd, -50, velocityUnits::pct);
-        }
-        else if(Controller.ButtonRight.pressing()) {
-            // clamp piston
-        }
-        else if(Controller.ButtonY.pressing()) {
-            // intake pistons + short eject with intake wheel motor
-            intake_wheels.spinFor(fwd, 100, msec); // might need to reverse & adjust time
+          intake_arm.spin(directionType::fwd, -25, velocityUnits::pct);
         }
         else {
           intake_arm.stop(vex::brakeType::hold);
           intake_wheels.stop(vex::brakeType::hold);
+          conveyor.stop(vex::brakeType::hold);
         }
         return intake;
     }
@@ -75,23 +88,15 @@ Arm_State arm_controls(Arm_State state) {
           return intake;
         }
         else if(Controller.ButtonR1.pressing()) {
-            right_arm.spin(directionType::fwd, 50, velocityUnits::pct);//raises right arm??? I have no idea if any of this works.I copied this from a youtube tutorial made in 2020 :(
+            right_arm.spin(directionType::fwd, 50, velocityUnits::pct);
+            left_arm.spin(directionType::fwd, 50, velocityUnits::pct);//raises right arm??? I have no idea if any of this works.I copied this from a youtube tutorial made in 2020 :(
         }
         else if(Controller.ButtonR2.pressing()) {
-            right_arm.spin(directionType::fwd, -50, velocityUnits::pct);//lowers right arm...maybe?
+            right_arm.spin(directionType::fwd, -50, velocityUnits::pct);
+            left_arm.spin(directionType::fwd, -50, velocityUnits::pct);//lowers right arm...maybe?
         }                                                               // im pretty sure its right
-        else if(Controller.ButtonL1.pressing()) {
-            left_arm.spin(directionType::fwd, 50, velocityUnits::pct); // okay either the left of right arm motor has to be reversed
-        }                                                               // we'll know when we test physically
-        else if(Controller.ButtonL2.pressing()) {
-            left_arm.spin(directionType::fwd, -50, velocityUnits::pct);
-        }
-        else if(Controller.ButtonRight.pressing()) {
-            // left claw piston i dunno if .open will keep it open and then if its already open u have to run the close function?
-            // going to research it more :|
-        }
         else if(Controller.ButtonY.pressing()) {
-            // right claw piston
+            // arm adjustment piston(s)
         }
         else {                    
           right_arm.stop(vex::brakeType::hold);
