9d54e33cce46101315d7d56876a958dc631a557d
areophix <Morgan.deng@icloud.com>
2025-01-17 22:59:54 -0600

    port updates

--- drivebase/src/robot_config.cpp
+++ drivebase/src/robot_config.cpp
@@ -11,19 +11,19 @@ brain Brain;
 controller Controller = controller(primary); // device names are usually capitilized cause lowercase ones are keywords
                                              // and the compiler will freak out cause it cant tell them apart
                                              
-motor LF = motor(PORT1, ratio6_1, true); // brain port the device is plugged into, cartridge ratio (different 
+motor LF = motor(PORT4, ratio6_1, true); // brain port the device is plugged into, cartridge ratio (different 
                                          // motors have diff cartridges for speed or strength), true = reversed, false = normal
-motor LB = motor(PORT2, ratio6_1, true);
-motor RF = motor(PORT3, ratio6_1, false);
-motor RB = motor(PORT4, ratio6_1, false);
+motor LB = motor(PORT5, ratio6_1, true);
+motor RF = motor(PORT6, ratio6_1, false);
+motor RB = motor(PORT7, ratio6_1, false);
 
-motor left_arm = motor(PORT5, ratio36_1, false);
-motor right_arm = motor(PORT6, ratio36_1, true);
+motor left_arm = motor(PORT19, ratio36_1, false);
+motor right_arm = motor(PORT20, ratio36_1, true);
 
-motor intake_arm = motor(PORT7, ratio6_1, true);
+motor intake_arm = motor(PORT2, ratio6_1, true);
 
-motor intake_wheels = motor(PORT8, ratio6_1, false);
-motor conveyor = motor(PORT9, ratio6_1, false);
+motor intake_wheels = motor(PORT1, ratio6_1, false);
+motor conveyor = motor(PORT11, ratio6_1, false);
 
 pneumatics clamp = pneumatics(Brain.ThreeWirePort.A);
 pneumatics arm_pistons = pneumatics(Brain.ThreeWirePort.B);
