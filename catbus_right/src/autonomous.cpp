1eebfb5450a5c43a4df39de5a16733cd846f04ee
areophix <Morgan.deng@icloud.com>
2025-01-29 22:28:26 -0600

    auton adjustments
    second ring is still trouble (first has 100% accuracy!) i dont think the visions actually seeing anything it keeps force quitting

--- catbus_right/src/autonomous.cpp
+++ catbus_right/src/autonomous.cpp
@@ -14,29 +14,31 @@ void autonomous() {
     //  drive(60, 60, 100);
     //drive(-60, -60, 25);
     this_thread::sleep_for(300);
-    conveyor.spinFor(350, msec, 150, vex::velocityUnits::pct);
+    conveyor.spin(fwd, 150, vex::velocityUnits::pct);
     this_thread::sleep_for(300);
     // drive(60, 60, 200);
     drive(60, -60, 600);
     drive_brake(brake);
     this_thread::sleep_for(150);
     intake_wheels.spin(fwd, 75, vex::velocityUnits::pct);
-    conveyor.spin(fwd, 150, vex::velocityUnits::pct);
     drive(70, 70, 575);
     cam.takeSnapshot(red, blue);
     Brain.resetTimer();
+    if(cam.objectCount >= 1) {Brain.Screen.print("objects detected");}
     while(cam.objectCount < 1) {
         cam.takeSnapshot(red, blue);
-        drive(40, 40, 75);
+        drive(20, 20, 75);
         this_thread::sleep_for(50);
         if(Brain.timer(sec) >= 3) {
             Brain.Screen.print("auto drive time out");
             break;
         }
     }
-    drive_brake(coast);
     intake_wheels.stop();
+    drive_brake(coast);
+    this_thread::sleep_for(300);
     conveyor.stop();
+    intake_wheels.spinFor(200, vex::timeUnits::msec, -75, vex::velocityUnits::pct);
 }
 // get side goal
 // score preset ring in goal
